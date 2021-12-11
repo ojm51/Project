643119a46ac8a12fc835286f86757fda4f906aca
chkang <chkangsc@gmail.com>
2021-12-11 17:53:59 +0900

    no message

--- predictSensorValue/app/src/main/java/com/example/myapplication/IntroActivity.java
+++ predictSensorValue/app/src/main/java/com/example/myapplication/IntroActivity.java
@@ -17,7 +17,7 @@ public class IntroActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), Home.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
